9bba91a00843bbc60c6c30c48972fc279897b92b
Luca Etienne Schulz <git@les.cx>
2023-09-15 18:42:46 +0200

    Move to Poetry package management
    - Move to proper package management with Poetry
    - Remove requirements.txt
    - Remove build.sh
    - Add simple .gitignore
    - Update language.py to reflect removement of all other languages
      except English

--- tidal_dl/lang/language.py
+++ tidal_dl/lang/language.py
@@ -2,59 +2,17 @@
 # -*- encoding: utf-8 -*-
 '''
 @File    :   language.py
-@Time    :   2020/08/19
-@Author  :   Yaronzz
-@Version :   1.0
-@Contact :   yaronhuang@foxmail.com
+@Time    :   2023/09/15
+@Author  :   les.cx
+@Version :   0.1
+@Contact :   luca@les.cx
 @Desc    :   
 '''
 
-from tidal_dl.lang.arabic import LangArabic
-from tidal_dl.lang.chinese import LangChinese
-from tidal_dl.lang.croatian import LangCroatian
-from tidal_dl.lang.czech import LangCzech
-from tidal_dl.lang.danish import LangDanish
-from tidal_dl.lang.dutch import LangDutch
 from tidal_dl.lang.english import LangEnglish
-from tidal_dl.lang.filipino import LangFilipino
-from tidal_dl.lang.french import LangFrench
-from tidal_dl.lang.german import LangGerman
-from tidal_dl.lang.hungarian import LangHungarian
-from tidal_dl.lang.italian import LangItalian
-from tidal_dl.lang.norwegian import LangNorwegian
-from tidal_dl.lang.polish import LangPolish
-from tidal_dl.lang.portuguese import LangPortuguese
-from tidal_dl.lang.russian import LangRussian
-from tidal_dl.lang.spanish import LangSpanish
-from tidal_dl.lang.turkish import LangTurkish
-from tidal_dl.lang.ukrainian import LangUkrainian
-from tidal_dl.lang.vietnamese import LangVietnamese
-from tidal_dl.lang.korean import LangKorean
-from tidal_dl.lang.japanese import LangJapanese
 
 _ALL_LANGUAGE_ = [
     ['English', LangEnglish()],
-    ['中文', LangChinese()],
-    ['Turkish', LangTurkish()],
-    ['Italian', LangItalian()],
-    ['Czech', LangCzech()],
-    ['Arabic', LangArabic()],
-    ['Russian', LangRussian()],
-    ['Filipino', LangFilipino()],
-    ['Croatian', LangCroatian()],
-    ['Spanish', LangSpanish()],
-    ['Portuguese', LangPortuguese()],
-    ['Ukrainian', LangUkrainian()],
-    ['Vietnamese', LangVietnamese()],
-    ['French', LangFrench()],
-    ['German', LangGerman()],
-    ['Danish', LangDanish()],
-    ['Hungarian', LangHungarian()],
-    ['Korean', LangKorean()],
-    ['Japanese', LangJapanese()],
-    ['Dutch', LangDutch()],
-    ['Polish', LangPolish()],
-    ['Norwegian', LangNorwegian()],
 ]
 
 class Language(object):
