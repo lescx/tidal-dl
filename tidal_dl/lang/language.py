4ff28a8d197bf0378b13b3225ff9fc2cb053c59d
Luca Etienne Schulz <git@les.cx>
2023-09-16 09:00:08 +0200

    Update language.py
    * Break down language.py to a minimum.

--- tidal_dl/lang/language.py
+++ tidal_dl/lang/language.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-'''
-@File    :   language.py
-@Time    :   2023/09/15
-@Author  :   les.cx
-@Version :   0.1
-@Contact :   luca@les.cx
-@Desc    :   
-'''
-
 from tidal_dl.lang.english import LangEnglish
-
-_ALL_LANGUAGE_ = [
-    ['English', LangEnglish()],
-]
 
 class Language(object):
     def __init__(self) -> None:
@@ -23,31 +9,10 @@ class Language(object):
         try:
             return int(str)
         except:
-            return 0
+            return 1
     
     def setLang(self, index):
         index = self.__toInt__(index)
-        if index >= 0 and index < len(_ALL_LANGUAGE_):
-            self.select = _ALL_LANGUAGE_[index][1]
-        else:
-            self.select = LangEnglish()
-
-    def getLangName(self, index):
-        index = self.__toInt__(index)
-        if index >= 0 and index < len(_ALL_LANGUAGE_):
-            return _ALL_LANGUAGE_[index][0]
-        return ""
-
-    def getLangChoicePrint(self):
-        array = []
-        index = 0
-        while True:
-            name = self.getLangName(index)
-            if name == "":
-                break
-            array.append('\'' + str(index) + '\'-' + name)
-            index += 1
-        return ','.join(array)
-
+        self.select = LangEnglish()
 
 LANG = Language()
